c2aa5258e11ea6d797e02dc4fe0bc96e77f16c4d
makjac <maks.jackowski@proton.me>
2025-02-24 00:47:20 +0100

    feat: initialize Firebase in main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_books_to_read/app/app.dart';
 import 'package:my_books_to_read/core/theme/theme_provider.dart';
+import 'package:my_books_to_read/firebase_options.dart';
 import 'package:my_books_to_read/injection_container.dart' as di;
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await di.init();
 
