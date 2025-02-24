fb429753f1fc1c9223bde94962467106958252ee
makjac <maks.jackowski@proton.me>
2025-02-25 00:14:32 +0100

    refactor: remove `SearchBooksresponse` model and add extension for `BookMatch` to get cover image URL

--- lib/pages/home/models/book_match/book_match.dart
+++ lib/pages/home/models/book_match/book_match.dart
@@ -5,32 +5,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'book_match.freezed.dart';
 part 'book_match.g.dart';
 
-SearchBooksresponse searchBooksresponseFromJson(String str) =>
-    SearchBooksresponse.fromJson(json.decode(str) as Map<String, dynamic>);
-
-String searchBooksresponseToJson(SearchBooksresponse data) =>
-    json.encode(data.toJson());
-
 BookMatch bookMatchFromJson(String str) =>
     BookMatch.fromJson(json.decode(str) as Map<String, dynamic>);
 
 String bookMatchToJson(BookMatch data) => json.encode(data.toJson());
-
-@freezed
-class SearchBooksresponse with _$SearchBooksresponse {
-  const factory SearchBooksresponse({
-    @JsonKey(name: 'numFound') int? numFound,
-    @JsonKey(name: 'start') int? start,
-    @JsonKey(name: 'numFoundExact') bool? numFoundExact,
-    @JsonKey(name: 'num_found') int? searchBooksresponseNumFound,
-    @JsonKey(name: 'q') String? q,
-    @JsonKey(name: 'offset') int? offset,
-    @JsonKey(name: 'docs') List<BookMatch>? docs,
-  }) = _SearchBooksresponse;
-
-  factory SearchBooksresponse.fromJson(Map<String, dynamic> json) =>
-      _$SearchBooksresponseFromJson(json);
-}
 
 @freezed
 class BookMatch with _$BookMatch {
@@ -47,4 +25,9 @@ class BookMatch with _$BookMatch {
 
   factory BookMatch.fromJson(Map<String, dynamic> json) =>
       _$BookMatchFromJson(json);
+}
+
+extension BookMatchX on BookMatch {
+  String get coverImageUrl =>
+      'https://covers.openlibrary.org/b/id/${coverI}-M.jpg';
 }
