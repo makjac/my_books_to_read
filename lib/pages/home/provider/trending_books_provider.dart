ad1300289f3e8e06bd4ca71f9420d24b35b75446
makjac <maks.jackowski@proton.me>
2025-02-24 21:39:20 +0100

    feat: add `TrendingBooksProvider` for managing trending books data

---
+++ lib/pages/home/provider/trending_books_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:my_books_to_read/core/utils/logger.dart';
+import 'package:my_books_to_read/pages/home/api/trending_books_api.dart';
+import 'package:my_books_to_read/pages/home/models/trending_books/trending_books.dart';
+
+class TrendingBooksProvider extends ChangeNotifier {
+  TrendingBooksProvider({TrendingBooksApi? trendingBooksApi})
+    : _trendingBooksApi = trendingBooksApi ?? TrendingBooksApiImpl();
+
+  final TrendingBooksApi _trendingBooksApi;
+
+  int _page = 1;
+  bool _isLoading = false;
+  final List<TrendingBook> _books = [];
+
+  int get page => _page;
+  bool get isLoading => _isLoading;
+  List<TrendingBook> get books => _books;
+
+  Future<void> fetchTrendingBooks() async {
+    if (_isLoading) return;
+
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final newBooks = await _trendingBooksApi.call(page: _page);
+
+      _books.addAll(newBooks);
+      _page++;
+    } catch (e) {
+      Logger.showLog(
+        'Error fetching trending books: $e',
+        'TrendingBooksProvider',
+      );
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
