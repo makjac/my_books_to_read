9112f61beed0a8e40fb132d2b2e74718d1b3ca89
makjac <maks.jackowski@proton.me>
2025-02-25 09:27:00 +0100

    refactor: update import path for BookThumbnail and adjust padding in HomeBooksGrid

--- lib/pages/home/widgets/grid/home_books_grid.dart
+++ lib/pages/home/widgets/grid/home_books_grid.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_books_to_read/core/widgets/book_thumbnail.dart';
 import 'package:my_books_to_read/pages/home/models/book_match/book_match.dart';
+import 'package:my_books_to_read/pages/home/widgets/thumbnail/book_thumbnail.dart';
 
 class HomeBooksGrid extends StatelessWidget {
   const HomeBooksGrid({
@@ -18,14 +18,13 @@ class HomeBooksGrid extends StatelessWidget {
     final crossAxisCount = (width / 280).clamp(1, 4).toInt();
 
     return SliverPadding(
-      padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+      padding: const EdgeInsets.fromLTRB(10, 8, 10, 16),
       sliver: SliverGrid(
         delegate: SliverChildBuilderDelegate((context, index) {
           if (index >= books.length) {
             return const _LoadingMoreIndicator();
           }
           return BookThumbnail(book: books[index]);
-          //
           // ignore: require_trailing_commas
         }, childCount: books.length + (isLoading ? 1 : 0)),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
