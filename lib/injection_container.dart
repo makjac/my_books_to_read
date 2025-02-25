fa5ccec70330a28d7a2eefa26dd42f3c58086a02
makjac <maks.jackowski@proton.me>
2025-02-25 03:09:04 +0100

    feat: register AuthProvider with FirebaseAuth in the injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
 import 'package:get_it/get_it.dart';
+import 'package:my_books_to_read/core/auth/auth_provider.dart';
 import 'package:my_books_to_read/pages/home/api/search_books_api.dart';
 import 'package:my_books_to_read/pages/home/api/trending_books_api.dart';
 import 'package:my_books_to_read/pages/home/provider/search_books_provider.dart';
