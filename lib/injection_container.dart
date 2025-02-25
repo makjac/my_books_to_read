aa2209c1076a8646cc951526dd40fa75ce06d2ed
makjac <maks.jackowski@proton.me>
2025-02-25 01:44:10 +0100

    feat: register SearchBooksApi and SearchBooksProvider in the dependency injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:my_books_to_read/pages/home/api/search_books_api.dart';
 import 'package:my_books_to_read/pages/home/api/trending_books_api.dart';
+import 'package:my_books_to_read/pages/home/provider/search_books_provider.dart';
 import 'package:my_books_to_read/pages/home/provider/trending_books_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
