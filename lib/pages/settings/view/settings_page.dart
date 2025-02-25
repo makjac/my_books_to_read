cdbbe8567faa60eab7adf389f38c70480df71530
makjac <maks.jackowski@proton.me>
2025-02-25 03:12:09 +0100

    chore: add button to navigate to authentication route from settings page

--- lib/pages/settings/view/settings_page.dart
+++ lib/pages/settings/view/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:my_books_to_read/core/router/app_router.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
@@ -9,7 +10,18 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: const Center(child: Text('Settings')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Settings'),
+            ElevatedButton(
+              onPressed: () => context.pushRoute(const AuthRoute()),
+              child: const Text('Go to Auth'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
