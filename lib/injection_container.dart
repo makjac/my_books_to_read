c62912ca84659755eee7ce77b8be1401cbbdbd71
makjac <maks.jackowski@proton.me>
2025-02-25 09:23:13 +0100

    feat: register SavedBooksRepository and SavedBooksProvider in the injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
 import 'package:get_it/get_it.dart';
@@ -8,6 +9,8 @@ import 'package:my_books_to_read/pages/home/api/search_books_api.dart';
 import 'package:my_books_to_read/pages/home/api/trending_books_api.dart';
 import 'package:my_books_to_read/pages/home/provider/search_books_provider.dart';
 import 'package:my_books_to_read/pages/home/provider/trending_books_provider.dart';
+import 'package:my_books_to_read/pages/saved_books/provider/saved_books_provider.dart';
+import 'package:my_books_to_read/pages/saved_books/repository/saved_books_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'injection_container.main.dart';
